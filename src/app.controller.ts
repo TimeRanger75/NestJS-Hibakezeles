aa8b30d9d5f73dad7860079da099ef55718c07b9
Csanad <Csanadsuli19@gmnail.com>
2023-02-01 16:21:54 +0100

    Patch kész

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,11 @@
 /* eslint-disable prettier/prettier */
-import { BadRequestException, Body, Controller, Get, HttpCode, Post, Render } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpCode, Param, Patch, Post, Render } from '@nestjs/common';
 import { DataSource } from 'typeorm';
 import { AppService } from './app.service';
 import RegisterDTO from './register.dto';
 import User from './user.entity';
 import * as bcrypt from 'bcrypt';
+import ChangeUserDto from './changeuser.dto';
 
 @Controller()
 export class AppController {
@@ -28,7 +29,7 @@ export class AppController {
       throw new BadRequestException('Email must contain a @ character')
     }
     if (registerdto.password!==registerdto.passwordagain) {
-      throw new BadRequestException("The two passwor must match")
+      throw new BadRequestException("The two password must match")
     }
     if (registerdto.password.length<8) {
       throw new BadRequestException('The password must be at least 8 characters long')
@@ -43,6 +44,23 @@ export class AppController {
     return user;
   }
 
-  
+  @Patch('/users/:id')
+  async updateUser(@Param('id')id:number, @Body()changeuser:ChangeUserDto){
+    if (!changeuser.email) {
+      throw new BadRequestException('No data has been changed')
+    }
+    if(!changeuser.email.includes('@')){
+      throw new BadRequestException('Email must contain a @ character')
+    }
+    if(changeuser.profilePicture.startsWith('http://')==false &&  changeuser.profilePicture.startsWith('https://')==false){
+      changeuser.profilePicture=null;
+    }
+    const userRepo=this.dataSource.getRepository(User);
+    userRepo.update({id:id}, {email:changeuser.email, profilePicture:changeuser.profilePicture})
+
+    return changeuser;
+
+  }
+
 
 }
